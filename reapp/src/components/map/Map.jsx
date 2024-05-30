cacc482343c6843a59430747a3006fc71a6e4d1b
brilu-22 <231214@virtualwindow.co.za>
2024-05-30 12:15:27 +0200

    commit

--- reapp/src/components/map/Map.jsx
+++ reapp/src/components/map/Map.jsx
@@ -1,25 +1,11 @@
 import React from "react";
-import "./map.scss";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Themap from "../../components/Themap/Themap";
 
 function Map() {
   return (
-    <MapContainer
-      center={[52.4797, -1.90269]}
-      zoom={13}
-      scrollWheelZoom={false}
-      className="map"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[52.4797, -1.90269]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map">
+      <Themap />
+    </div>
   );
 }
 
