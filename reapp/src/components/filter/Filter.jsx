854b297f194991144fcddd4c2d26616148e40ad0
brilu-22 <231214@virtualwindow.co.za>
2024-05-30 03:00:14 +0200

    commit

---
+++ reapp/src/components/filter/Filter.jsx
@@ -0,0 +1,88 @@
+import "./filter.scss";
+
+function Filter() {
+  return (
+    <div className="filter">
+      <h1>
+        Search Results for <b>Cape Town</b>{" "}
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="type">text</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">any</option>
+            <option value="buy">Apartment</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">MaxPrice</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="text" id="bedroom" name="bedroom" placeholder="any" />
+        </div>
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          />
+        </div>
+
+        <button>
+          <img src="/locate.png" alt="" width={29} height={29} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
--- reapp/src/routes/homepage/listPage/ListPage.jsx
+++ reapp/src/routes/homepage/listPage/ListPage.jsx
@@ -1,7 +1,24 @@
+import Filter from "../../../components/filter/Filter";
 import "./listpage.scss";
+import { listData } from "../../../lib/dummydata";
+import Card from "../../../components/card/Card";
 
 function ListPage() {
-  return <div className="List">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="List">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 }
 
 export default ListPage;
